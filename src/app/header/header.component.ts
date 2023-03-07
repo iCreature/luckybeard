1ebc8acabdab85b1462e63a7e602f11396a8998e
iCreature <50922474+iCreature@users.noreply.github.com>
2023-03-07 20:22:46 +0200

    Done!

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {  Component,ElementRef, OnInit,EventEmitter, Output ,Input} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor(private router: Router, private el: ElementRef){
+
+  }
+  externalUrl = 'https://www.luckybeard.com/';
+  @Input()
+  onButtonClick!: Function;
+  @Output() btnBuyer = new EventEmitter();
+
+   // dealership login
+  dealershipLogin(): void {
+    window.open(this.externalUrl,'_blank');
+  }
+
+  // scroll to buyers
+  scrollToBuyer() {
+    this.btnBuyer.emit();
+    this.onButtonClick();
+  }
+
+  // scroll to private seller
+  onSellerButton(): void{
+    const section = this.el.nativeElement.querySelector('#buyer-id');
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  // scroll to immediate response
+  onResponseButton(): void {
+    const section = this.el.nativeElement.querySelector('#buyer-id');
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component,ElementRef, OnInit  } from '@angular/core';
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+
+   constructor(private el: ElementRef, private router: Router){
+
+  }
+  externalUrl = 'https://www.luckybeard.com/';
+  ngOnInit() {}
+
+     // dealership login
+  dealershipLogin(): void {
+      window.open(this.externalUrl,'_blank');
+  }
+
+  //scroll to buyer section
+   // scroll to buyers
+   scrollToBuyer(): void {
+    const section = this.el.nativeElement.querySelector('#buyer-id');
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  // scroll to private seller
+  scrollToPrivateSeller(): void{
+    const section = this.el.nativeElement.querySelector('#buyer-id');
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  // scroll to immediate response
+  scrollToResponse(): void {
+    const section = this.el.nativeElement.querySelector('#buyer-id');
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 
 }
